8c7c2e75a4ff43e013e1e6fc30c7a230fd57da65
Mark Papadakis <mpapadakis@phaistosnetworks.gr>
2019-11-10 12:51:02 +0200

    Refactoring

--- service_main.cpp
+++ service_main.cpp
@@ -2,23 +2,6 @@
 #include <sys/time.h>
 #include <sys/resource.h>
 
-int log_fd;
-
-void init_log() {
-#ifndef LEAN_SWITCH
-        log_fd = open("/tmp/tank_srv.log", O_WRONLY | O_CREAT | O_LARGEFILE | O_APPEND, 0775);
-
-        if (log_fd != -1) {
-                Buffer m;
-
-                m.append(Date::ts_repr(time(nullptr)), " TANK service initialization in progress\n");
-                write(log_fd, m.data(), m.size());
-        }
-#else
-        log_fd = -1;
-#endif
-}
-
 // deals with the weak symbol getentropy@@GLIBC_2.25
 int getentropy(void *buffer, size_t length) {
         return 0;
@@ -30,8 +13,6 @@ int main(int argc, char *argv[]) {
         if (struct rlimit rl{.rlim_cur = RLIM_INFINITY, .rlim_max = RLIM_INFINITY}; setrlimit(RLIMIT_CORE, &rl) == -1) {
                 Print("Warning: setrlimit() failed:", strerror(errno), "\n");
         }
-
-        init_log();
 
         return Service{}.start(argc, argv);
 }
