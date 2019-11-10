27853f1d0dc1b8f540dcf3bd57781fd956bc49ef
Mark Papadakis <mpapadakis@phaistosnetworks.gr>
2019-11-10 12:48:39 +0200

    Removed test*cpp for now

--- test_service_util.cpp
+++
@@ -1,8 +0,0 @@
-#include <ext/Catch/catch.hpp>
-#include "service.h"
-
-TEST_CASE("Utilities") {
-	REQUIRE(TANKUtil::minimum(100,10,50,2,70,80) == 2);
-	REQUIRE(TANKUtil::minimum(10,20,30,40,50) == 10);
-	REQUIRE(TANKUtil::minimum(100, 10, 50, 70, 2) == 2);
-}
